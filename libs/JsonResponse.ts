6694cf71d6a3b4c53ed56ad6849add668b904f3f
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-20 10:31:24 +0100

    Bug fixed

--- libs/JsonResponse.ts
+++ libs/JsonResponse.ts
@@ -4,8 +4,13 @@
 
 
 export class JsonResponse extends Response {
-    constructor(text: string, init?: ResponseInit) {
-        super(text, init);
+    constructor(data: unknown, init?: ResponseInit, prettyPrint?: boolean) {
+        const body = (() => {
+            if (prettyPrint) return JSON.stringify(data, null, 2);
+            return JSON.stringify(data);
+        })();
+
+        super(body, init);
 
         this.headers.set("Content-Type", "application/json; charset=utf-8");
     }
